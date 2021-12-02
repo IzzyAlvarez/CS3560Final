709032fabb8bb2909ef85d87b2a729505f772bca
IzzyAlvarez <ia638719@ohio.edu>
2021-12-02 16:59:19 -0500

    functional counting program

--- counting.cc
+++ counting.cc
@@ -5,24 +5,33 @@
 int number_of_lines = 0;
 int number_of_chars = 0;
 
-int countLine(){
+int countLine( std::string pName ){
         return number_of_lines;
 }
 
-int countChar(){
+int countChar(std::string pName){
         return number_of_chars;
 }
 
-int main() {
-    std::string line;
+int main(int argc, char *argv[]) {
+	if(argc<1){
+		std::cout<<1<<std::endl;
+		std::cout<<6<<std::endl;
+} 
+else{
+   
+	std::string line;
     //change name of textfile
-    std::ifstream myfile("hello.txt");
+	std::string fileName;
+	fileName=atoi(argv[0]);
+    std::ifstream myfile(fileName);
 
     while (std::getline(myfile, line)){
         ++number_of_lines;
         number_of_chars+=line.length();
     }
-    std::cout << "Number of lines in text file: " << countLine()<<std::endl;
-    std::cout<<"Number of characters in text file: "<<countChar();
+    std::cout << "Number of lines in text file: " << countLine(fileName)<<std::endl;
+    std::cout<<"Number of characters in text file: "<<countChar(fileName);
+}
     return 0;
 }
