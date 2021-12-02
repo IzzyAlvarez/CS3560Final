114e86a8486fff0f4211275a1d8d4b00abd33bf0
IzzyAlvarez <ia638719@ohio.edu>
2021-12-02 16:50:38 -0500

    created functions countChar() and countLine()

--- counting.cc
+++ counting.cc
@@ -2,17 +2,27 @@
 #include <fstream>
 #include <string>
 
+int number_of_lines = 0;
+int number_of_chars = 0;
+
+int countLine(){
+        return number_of_lines;
+}
+
+int countChar(){
+        return number_of_chars;
+}
+
 int main() {
-    int number_of_lines = 0, c;
     std::string line;
     //change name of textfile
     std::ifstream myfile("hello.txt");
 
     while (std::getline(myfile, line)){
         ++number_of_lines;
-        c+=line.length();
+        number_of_chars+=line.length();
     }
-    std::cout << "Number of lines in text file: " << number_of_lines<<std::endl;
-    std::cout<<"Number of characters in text file: "<<c;
+    std::cout << "Number of lines in text file: " << countLine()<<std::endl;
+    std::cout<<"Number of characters in text file: "<<countChar();
     return 0;
 }
